2c262b9c16babc90f7d6de8e90ffa12e992d90eb
Borja <borja@antweb.es>
2018-04-24 12:43:23 +0200

    Fix path and configure enviroment pagespeed and https

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,12 +4,16 @@
 envsubst '\$APP_ENV \$APP_DEBUG \$HTTPS_FORCE' < /usr/local/nginx/nginx.tmpl > /usr/local/nginx/conf/nginx.conf
 
 #php environment variables
-envsubst '\$TIMEZONE' < /etc/php/7.1/php-${APP_ENV}.ini.tmpl > /etc/php/7.1/cli/conf.d/50-setting.ini
-envsubst '\$TIMEZONE' < /etc/php/7.1/php-${APP_ENV}.ini.tmpl > /etc/php/7.1/fpm/conf.d/50-setting.ini
+envsubst '\$TIMEZONE' < /etc/php/7.1/php-${APP_ENV}.tmpl > /etc/php/7.1/cli/conf.d/50-setting.ini
+envsubst '\$TIMEZONE' < /etc/php/7.1/php-${APP_ENV}.tmpl > /etc/php/7.1/fpm/conf.d/50-setting.ini
 
-#redirection http to https if HTTPS_FORCE is enabled
+#enable https redicection and pagespeed in prod mode
 if [[ "$HTTPS_FORCE" = "off" ]]; then
     cat "" > /usr/local/nginx/https_force.conf
+fi
+
+if [[ "$APP_ENV" = "dev" ]]; then
+    cat "" > /usr/local/nginx/pagespeed.conf
 fi
 
 #Causes the shell to exit if any subcommand or pipeline returns a non-zero status.
